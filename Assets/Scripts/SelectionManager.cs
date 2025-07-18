d43914c2a78521fb68cd508d474bcd0ab3c943c1
Isaac <isaachj06@gmail.com>
2025-07-18 03:53:59 -0400

    Trees

---
+++ Assets/Scripts/SelectionManager.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class SelectionManager : MonoBehaviour
+{
+
+    public GameObject interaction_Info_UI;
+    Text interaction_text;
+ 
+    private void Start()
+    {
+        interaction_text = interaction_Info_UI.GetComponent<Text>();
+    }
+ 
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            var selectionTransform = hit.transform;
+ 
+            if (selectionTransform.GetComponent<InteractableObject>())
+            {
+                interaction_text.text = selectionTransform.GetComponent<InteractableObject>().GetItemName();
+                interaction_Info_UI.SetActive(true);
+            }
+            else 
+            { 
+                interaction_Info_UI.SetActive(false);
+            }
+ 
+        }
+    }
+    
+}
